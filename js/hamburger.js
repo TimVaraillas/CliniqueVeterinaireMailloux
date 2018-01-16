c7f6d3e59865d0035d33bcfc42f8955aa7af7e59
Timothée Varaillas <timothee.varaillas@gmail.com>
2018-01-16 09:22:58 +0100

    Responsive tablette

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,13 +2,14 @@
     function toggleMenu() {
         $('.bar').toggleClass('animate');
         $('.navbar-hamburger-menu').toggleClass('navbar-hamburger-menu-active');
+        $('body').toggleClass('overflowHidden');
     }
 
     $('.hamburger-menu').on('click', function () {
-        toggleMenu()
+        toggleMenu();
     });
     $('.hamburger-link').on('click', function () {
-        toggleMenu()
+        toggleMenu();
     });
 })();
 
